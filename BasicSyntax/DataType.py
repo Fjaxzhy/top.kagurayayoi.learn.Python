08c1719d63553b94f39aacb57b01ff0dead299ce
kagurayayoi <862883646@qq.com>
2021-03-30 20:24:04 +0800

    基础数据类型 DataType

---
+++ BasicSyntax/DataType.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Python变量不需要声明数据类型
+# 变量在使用前必须赋值
+# 变量没有类型 类型指内存中对象的类型
+
+# 不可变数据 Number / String / Tuple
+# 可变数据   List / Dictionary / Set
+
+# 数字 Number
+#   整数 Int
+IntNum = 100
+#   浮点数 Float
+FloatNum = 100.10
+#   布尔值 Boolean // True:1 False:0
+BoolNum = True
+#   复数 Complex
+ComplexNum = 1.00j
+
+# 字符串 String
+Str = "这是字符串"
+
+# 列表 List
+List = ['a', 'b', 1, 2]
+
+# 元组 Tuple
+Tup = ('a', 'b', 1, 2)
+
+# 集合 Set
+Set = {'a', 'b', 1, 2}
+
+# 字典 Dictionary
+Dict = {'key1': 'value1', 'key2': 'value2'}
